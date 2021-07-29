74c3b02c66cc7deb58b61f30fe0fd62b92add906
DerickPCLinux <derickdev6@gmail.com>
2021-07-29 16:57:56 -0500

    Punteros

--- main.c
+++ main.c
@@ -10,5 +10,23 @@ int main(void)
 
     printf("num: %d \n", num);
 
+    // Lo que hacemos aca es crear en HEAP una estructura que es
+    // un conjunto de datos, el cual se crea con el constructor,
+    // retornando a la variable var la direccion del objeto en memoria,
+    // var es el puntero
+    // class_name var = new class_name();
+
+    // Si quiero almacenar en una variable la direccion de otra variable
+    // hago lo siguiente - esto es un puntero
+    // El operador & me permite obtener la direccion de una variable
+    int *pvar = &num;
+
+    printf("num dir: %lx \n", (long int)&num);
+    printf("pvar: %lx \n", (long int)pvar);
+
+    // Esto es una variable donde almaceno direcciones donde almaceno variables
+    // que almacenan direcciones, de variables que almacenan enteros
+    // int **ppvar = &*pvar;
+
     return EXIT_SUCCESS;
 }
