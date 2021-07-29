ef7b1c91abbd7a371a070971cc7a1a2fb7bec48f
DerickPCLinux <derickdev6@gmail.com>
2021-07-29 16:28:37 -0500

    Basic C program

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+
+    printf("Sistemas Operativos\n");
+
+    int num = 10;
+
+    printf("num: %d \n", num);
+
+    return EXIT_SUCCESS;
+}
