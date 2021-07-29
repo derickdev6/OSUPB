6f8054eaf9357352eae140f526abe991c972e62c
DerickPCLinux <derickdev6@gmail.com>
2021-07-29 17:07:11 -0500

    Usando punteros para leer y escribir una direccion en memoria

--- main.c
+++ main.c
@@ -26,7 +26,19 @@ int main(void)
 
     // Esto es una variable donde almaceno direcciones donde almaceno variables
     // que almacenan direcciones, de variables que almacenan enteros
-    // int **ppvar = &*pvar;
+    // int **ppvar = &pvar;
+
+    // Cosas que puedo hacer con el puntero son
+    // Leer la variable usando el puntero
+    // Con el * puedo leer el contenido de la direccion almacenada en pvar
+
+    printf("Leer num usando pvar -> %d\n", *pvar);
+
+    // Escribir en la variable usando pvar
+
+    *pvar = 20;
+
+    printf("Escribi 20 en num usando pvar -> %d\n", *pvar);
 
     return EXIT_SUCCESS;
 }
