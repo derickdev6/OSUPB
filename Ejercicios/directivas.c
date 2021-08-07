b32fd1dc664b1a7ec3c4558d25ce72f820044308
DerickPCLinux <derickdev6@gmail.com>
2021-08-07 18:49:56 -0500

    Directivas, function

--- Ejercicios/directivas.c
+++ Ejercicios/directivas.c
@@ -4,14 +4,12 @@
 // Directivas
 #define M 5
 #define C 5
+#define MAP(val, m, c) val *m + c
 
 int main(int argc, char *argv[])
 {
     int x = 2;
-    // La variable y sera definida por x * M + C
-    // M sera dada por la directiva en la linea 5
-    // C sera dada por la directiva en la linea 6
-    int y = x * M + C;
+    int y = MAP(x, M, C);
     printf("y = %d\n", y);
     return 0;
 }
