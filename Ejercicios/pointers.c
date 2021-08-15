33fae1b13d44715f8a1a9eb192485ef058a16159
DerickPCLinux <derickdev6@gmail.com>
2021-08-15 17:31:12 -0500

    Pointers v2

--- Ejercicios/pointers.c
+++ Ejercicios/pointers.c
@@ -5,9 +5,13 @@
 int main(int argc, char *argv[])
 {
     uint32_t var = 10;
+    // Almacenamos en una variable de puntero la direccion de otra variable
     uint32_t *pvar = &var;
 
+    // Verificamos que
+    // Las direcciones son la misma
     printf("&var %d = pvar %d\n", &var, pvar);
+    // Los valores en la direccion son accesibles con ambas variables
     printf("var %d = *pvar %d\n", var, *pvar);
 
     *pvar = 20;
