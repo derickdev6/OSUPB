be968c6bf38dd1da37bbf7fd226432e0aed71963
DerickPCLinux <derickdev6@gmail.com>
2021-08-16 17:11:06 -0500

    IO, solved enter error

--- Ejercicios/io.c
+++ Ejercicios/io.c
@@ -6,6 +6,9 @@ int main(void)
     printf("What is your name? ");
     if (fgets(name, 10, stdin) != NULL)
     {
+        // Esta linea elimina el caracter de enter,
+        // Reemplazandolo por un 0
+        name[strlen(name) - 1] = 0;
         printf("Hello %s!\n", name);
     }
 }
